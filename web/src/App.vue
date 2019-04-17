50e34831542f8ae382bdd3edabb943d660e3604a
sumitra komlue <31061208+faijasmine@users.noreply.github.com>
2019-04-18 01:30:37 +0700

    edit3

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-app style=" background: #3A1C71;background: linear-gradient(to right, #FFAF7B, #D76D77, #3A1C71);" >
-    <v-toolbar color="#009688" app >
-      <v-toolbar-title class="headline text lime--text" >
-        LIGTH
+  <v-app style="background: #3A1C71;background: linear-gradient(to right, #FFAF7B, #D76D77, #3A1C71);" >
+    <v-toolbar color="#FFCCBC" app >
+      <v-toolbar-title style="font-family: Comic Sans MS, serif; font-size:20pt">
+        LIGHT CHAT
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
@@ -11,22 +11,25 @@
     </v-toolbar>
    
    <!-- content -->
-    <v-content>
+    <v-content grid-list-md> 
       <!-- <HelloWorld/> -->
-        <v-list style="background:#FFCCBC;
-                      width:auto;
-                      margin:auto;">
-          <v-list-tile v-for="(item,index) in chat" :key="index">
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item"></v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+      
+       <v-layout row >
+      
+        
+        <v-flex xs6>
+        <v-card style="  width: 550px;
+                         padding: 50px;
+                         margin: auto;
+                         margin-top:120px;
+                         border-radius: 5px;">
+                   
         <v-form v-model="valid" v-on:submit.prevent="send">
           <v-container class="back">
             <v-layout>
               <v-flex xs12 md12>
                 <v-text-field
+                  :counter="30"
                   :disabled="!canSend"
                   v-model="message"
                   :label="canSend?'text me!!':'sending...'"
@@ -36,7 +39,28 @@
             </v-layout>
           </v-container>
         </v-form>
-      
+      </v-card>
+        </v-flex>
+
+        <v-flex xs6>
+          
+        <v-card style="  background:#FFCCBC;
+                         width: 550px;
+                         margin: auto;
+                         margin-top:120px;
+                         border-radius: 5px;">
+        <v-list>
+          <v-list-tile v-for="(item,index) in chat" :key="index">
+            <v-list-tile-content>
+              <v-list-tile-title v-text="item"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        </v-card>
+      </v-flex>
+
+      </v-layout>
+     
     </v-content>
    
     
@@ -83,7 +107,7 @@ export default {
   },
   data() {
     return {
-      chat: ['cfghcfh'],
+      chat: ["cfghcfh", "fhskkjllkjkljn","dfghjkl;"],
       canSend: true,
       valid: false,
       message: ""
